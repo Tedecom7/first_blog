18b9581799e85abb977d267815e15dd46fe049b5
Tedecom7 <tedecom7@gmail.com>
2020-10-04 23:28:10 +0200

    poprawiony

--- blogdj/settings.py
+++ blogdj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zfpuf2p##xj!n4b#t3&gecsz$y%u6b*vwe+*hnneub22pyckq3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
